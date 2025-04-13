ba640741bff41e14e178d1a88e17f91e58e89487
Helmasur <none@none.com>
2025-04-13 15:59:46 +0200

    string formatting

--- gui.gd
+++ gui.gd
@@ -25,21 +25,26 @@ func _process(_delta: float) -> void:
 		%UnitStats.visible = true
 		%BuildControls.visible = false
 		%TriCon.set_light_mode(true)
-		%Energy.text = "%.3f / %.3f" % [selected_unit.energy, selected_unit.energy_max]
-		%Energy_prog.value = selected_unit.energy / selected_unit.energy_max
-		%Power.text = "P: %.3f" % selected_unit.power
-		%Speed.text = "S: %.3f" % selected_unit.speed
-		%Health.text = "D: %.3f" % selected_unit.defense
+		%Energy.text = "Energy: " + _fmt(selected_unit.energy).lstrip(" ") + "/" + _fmt(selected_unit.energy_max).lstrip(" ")
+		#%Energy_prog.value = selected_unit.energy / selected_unit.energy_max
+		%Power.text = "P: " + _fmt(selected_unit.power)
+		%Speed.text = "S: " + _fmt(selected_unit.speed)
+		%Health.text = "D: " + _fmt(selected_unit.defense)
 		%TriCon.set_point(selected_unit.defense_prop, selected_unit.power_prop, selected_unit.speed_prop)
 	else:
 		%UnitStats.visible = false
 		%BuildControls.visible = true
 		%TriCon.set_light_mode(false)
-		%BuildEnergy.text = "Cost: %.0f" % %EnergySlider.value
-		%BuildArmour.text = "A: %.0f" % (%TriCon.current_h * %EnergySlider.value)
-		%BuildPower.text = "P: %.0f" % (%TriCon.current_p * %EnergySlider.value)
-		%BuildSpeed.text = "S: %.0f" % (%TriCon.current_s * %EnergySlider.value)
-	
+		%BuildEnergy.text = "Cost: " + _fmt(%EnergySlider.value)
+		%BuildArmour.text = "A: " + _fmt(%TriCon.current_h * %EnergySlider.value)
+		%BuildPower.text = "P: " + _fmt(%TriCon.current_p * %EnergySlider.value)
+		%BuildSpeed.text = "S: " + _fmt(%TriCon.current_s * %EnergySlider.value)
+
+func _fmt(f: float) -> String:
+	if f < 10:
+		return ("%6.1f" % f)
+	else:
+		return ("%6.0f" % f)
 
 func _on_transform_button_pressed() -> void:
 	#print("Tricon: ", %TriCon.current_h, " ", %TriCon.current_p, " ", %TriCon.current_s)
